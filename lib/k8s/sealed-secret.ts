bcee806135eb106961fb6855540870f680a91f6d
alex weidner <side.weidner@gmail.com>
2020-05-19 13:46:05 -0400

    support clusterwide/namespace wide sealedsecret annos

--- lib/k8s/sealed-secret.ts
+++ lib/k8s/sealed-secret.ts
@@ -1,3 +1,4 @@
+import { V1ObjectMeta } from '@kubernetes/client-node';
 import { merge } from 'lodash-es';
 import { SecretTypes } from './secrets';
 
@@ -13,6 +14,13 @@ interface SealedSecretOptions {
       annotations?: { [prop: string]: string };
     };
   };
+  clusterWide?: boolean;
+  namespaceWide?: boolean;
+}
+
+export enum SealedSecretAnnotations {
+  clusterWide = 'sealedsecrets.bitnami.com/cluster-wide',
+  namespaceWide = 'sealedsecrets.bitnami.com/namespace-wide',
 }
 
 const defaults = {
@@ -25,11 +33,24 @@ const defaults = {
 // TODO: have secret inherit labels of sealedsecret regardless
 export const sealedSecret = (name: string, opts: SealedSecretOptions) => {
   const { encryptedData, template } = merge({}, defaults, opts);
+  const metadata: V1ObjectMeta = { name };
+
+  if (opts.clusterWide) {
+    metadata.annotations = {
+      [SealedSecretAnnotations.clusterWide]: 'true',
+    };
+  }
+
+  if (opts.namespaceWide) {
+    metadata.annotations = {
+      [SealedSecretAnnotations.namespaceWide]: 'true',
+    };
+  }
 
   return {
     kind,
     apiVersion,
-    metadata: { name },
+    metadata,
     spec: { encryptedData, template },
   };
 };
