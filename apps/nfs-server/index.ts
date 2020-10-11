486d0baf4bc46deb9881d1620f6f0ba459169d58
alex weidner <side.weidner@gmail.com>
2020-10-11 13:01:30 -0400

    Revert "refactor(apps): make all apps use default exports"
    This reverts commit b038191f04f25b10f2ba571793b8a6a369939f29.

--- apps/nfs-server/index.ts
+++ apps/nfs-server/index.ts
@@ -1,10 +1,10 @@
 import { valuesForGenerate } from '@jkcfg/kubernetes/generate';
-import NfsServer from './lib';
+import { nfsServer } from './lib';
 
 /**
  * `jkcfg generate` friendly export, can provide parameters via command line
  */
-export default valuesForGenerate(NfsServer());
+export default valuesForGenerate(nfsServer());
 
 /**
  * Export for TypeScript/JavaScript consumers and other libraries rendered
