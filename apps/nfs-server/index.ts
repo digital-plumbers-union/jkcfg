8b30307629b289e4dee267110a0320c310c3abc1
alex weidner <side.weidner@gmail.com>
2020-10-11 02:32:17 -0400

    fix(nfs-server): update app structure correctly

--- apps/nfs-server/index.ts
+++ apps/nfs-server/index.ts
@@ -1,76 +1,13 @@
-import {
-  appNameSelector,
-  Container,
-  Deployment,
-  finalize,
-  svcPort,
-  VolumeTypes,
-} from '@dpu/jkcfg-k8s';
-import * as k8s from '@jkcfg/kubernetes/api';
-import { merge } from 'lodash-es';
-import { Parameters, params } from './params';
+import { valuesForGenerate } from '@jkcfg/kubernetes/generate';
+import { nfsServer } from './lib';
 
-const nfsServer = (p?: Parameters) => {
-  const {
-    name,
-    namespace,
-    image,
-    clusterIP,
-    port,
-    servicePort,
-    hostPath,
-    nodeSelector,
-    serviceType,
-    replicas,
-  } = merge({}, params, p);
-  const selector = appNameSelector(name);
+/**
+ * `jkcfg generate` friendly export, can provide parameters via command line
+ */
+export default valuesForGenerate(nfsServer());
 
-  const svc = new k8s.core.v1.Service(name, {
-    spec: {
-      clusterIP,
-      ports: [svcPort(servicePort ? servicePort : port, { targetPort: port })],
-      selector,
-      type: serviceType,
-    },
-  });
-
-  const deploy = Deployment(name, { labels: selector, replicas });
-  const volumeName = 'host-path';
-  const mountPath = '/share';
-  // TODO: make `type: 'Directory'` default
-  deploy.addVolume(volumeName, VolumeTypes.hostPath, {
-    path: hostPath,
-    type: 'Directory',
-  });
-  const serverContainer = Container({
-    name,
-    image,
-    port,
-    env: {
-      SHARED_DIRECTORY: mountPath,
-    },
-  });
-  // TODO: container cant handle non-PVC volumeMounts
-  serverContainer.volumeMounts = [
-    {
-      mountPath,
-      name: volumeName,
-    },
-  ];
-  serverContainer.securityContext = {
-    capabilities: {
-      add: ['SYS_ADMIN', 'SETPCAP', 'SYS_RESOURCE', 'DAC_READ_SEARCH'],
-    },
-  };
-  deploy.addContainer(serverContainer);
-  if (nodeSelector) {
-    // TODO: more poor types from parameters
-    deploy.deployment.spec!.template!.spec!.nodeSelector = nodeSelector as {
-      [prop: string]: string;
-    };
-  }
-
-  return finalize([svc, deploy.resource], { labels: selector, namespace });
-};
-
-export default nfsServer;
+/**
+ * Export for TypeScript/JavaScript consumers and other libraries rendered
+ * via `jkcfg generate`
+ */
+export * from './lib';
---
+++ apps/nfs-server/lib.ts
@@ -0,0 +1,74 @@
+import {
+  appNameSelector,
+  Container,
+  Deployment,
+  finalize,
+  svcPort,
+  VolumeTypes,
+} from '@dpu/jkcfg-k8s';
+import * as k8s from '@jkcfg/kubernetes/api';
+import { merge } from 'lodash-es';
+import { Parameters, params } from './params';
+
+export const nfsServer = (p?: Parameters) => {
+  const {
+    name,
+    namespace,
+    image,
+    clusterIP,
+    port,
+    servicePort,
+    hostPath,
+    nodeSelector,
+    serviceType,
+    replicas,
+  } = merge({}, params, p);
+  const selector = appNameSelector(name);
+
+  const svc = new k8s.core.v1.Service(name, {
+    spec: {
+      clusterIP,
+      ports: [svcPort(servicePort ? servicePort : port, { targetPort: port })],
+      selector,
+      type: serviceType,
+    },
+  });
+
+  const deploy = Deployment(name, { labels: selector, replicas });
+  const volumeName = 'host-path';
+  const mountPath = '/share';
+  // TODO: make `type: 'Directory'` default
+  deploy.addVolume(volumeName, VolumeTypes.hostPath, {
+    path: hostPath,
+    type: 'Directory',
+  });
+  const serverContainer = Container({
+    name,
+    image,
+    port,
+    env: {
+      SHARED_DIRECTORY: mountPath,
+    },
+  });
+  // TODO: container cant handle non-PVC volumeMounts
+  serverContainer.volumeMounts = [
+    {
+      mountPath,
+      name: volumeName,
+    },
+  ];
+  serverContainer.securityContext = {
+    capabilities: {
+      add: ['SYS_ADMIN', 'SETPCAP', 'SYS_RESOURCE', 'DAC_READ_SEARCH'],
+    },
+  };
+  deploy.addContainer(serverContainer);
+  if (nodeSelector) {
+    // TODO: more poor types from parameters
+    deploy.deployment.spec!.template!.spec!.nodeSelector = nodeSelector as {
+      [prop: string]: string;
+    };
+  }
+
+  return finalize([svc, deploy.resource], { labels: selector, namespace });
+};
