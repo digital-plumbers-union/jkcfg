7d2e39a3f2e9f94b9692ab995bc0b14d6f2c0b35
alex weidner <side.weidner@gmail.com>
2020-10-11 00:21:08 -0400

    refactor(monero-node): move to new apps/ structure, use new parameters

---
+++ apps/monero-node/lib.ts
@@ -0,0 +1,145 @@
+import {
+  appNameSelector,
+  Container,
+  Deployment,
+  finalize,
+  KubernetesObject,
+  metaLabels,
+  PVC,
+  sealedSecret,
+  svcPort,
+} from '@dpu/jkcfg-k8s';
+import * as k8s from '@jkcfg/kubernetes/api';
+import { isUndefined, merge } from 'lodash-es';
+import { Parameters, params } from './params';
+
+/**
+ * Renders a deployable set of manifests for a Monero node.
+ * @param p
+ */
+const monerod = (p?: Partial<Parameters>): KubernetesObject[] => {
+  const {
+    name,
+    namespace,
+    ingress,
+    persistence,
+    port,
+    image,
+    sealedSecrets,
+    secrets,
+  } = merge({}, params, p);
+
+  const selector = appNameSelector(name);
+
+  const createSecrets = secrets.rpcCreds || secrets.walletPass;
+
+  const resources: KubernetesObject[] = [
+    PVC(name, {
+      // TODO: persistence.size has to be NonNulled via `!`
+      //       should create some type of NonNullable<> assertion?
+      size: persistence.size!,
+      storageClass: persistence.storageClass!,
+    }),
+    new k8s.core.v1.Service(name, {
+      spec: {
+        ports: [svcPort(port)],
+        // default behavior of App is to add standard kube app label with
+        // string provided, so we can rely on that for the service selector
+        selector,
+      },
+    }),
+  ];
+
+  if (createSecrets) {
+    const secretData: { [prop: string]: string } = {};
+    for (const secretName in secrets) {
+      if (secrets[secretName] !== undefined) {
+        secretData[secretName] = secrets[secretName];
+      }
+    }
+
+    resources.push(
+      sealedSecrets
+        ? sealedSecret(name, {
+            encryptedData: secretData,
+            // ensures result secret is discoverable via app label
+            // until sealed-secret module ensures secrets inherit labels
+            template: { ...metaLabels(selector) },
+          })
+        : new k8s.core.v1.Secret(name, { stringData: secretData })
+    );
+  }
+
+  const deploy = Deployment(name, { labels: selector });
+  deploy.addVolume(name);
+  const moneroContainer = Container({
+    name,
+    image,
+    port,
+    pvcs: [
+      {
+        name,
+        mountPath: '/monero',
+      },
+    ],
+    command: ['monerod'],
+    args: [
+      '--data-dir',
+      '/monero',
+      '--log-level',
+      '0',
+      '--non-interactive',
+      '--rpc-bind-ip',
+      '0.0.0.0',
+      '--restricted-rpc',
+      '--confirm-external-bind',
+      '--block-sync-size',
+      '100',
+    ],
+    resources: {
+      limits: {
+        memory: '2Gi',
+      },
+    },
+  });
+  if (createSecrets) moneroContainer.envFrom = [{ secretRef: { name } }];
+  deploy.addContainer(moneroContainer);
+  resources.push(deploy.resource);
+
+  // ingress
+  if (ingress.enabled) {
+    const { annotations, host, tls } = ingress;
+    // TODO: make this a function
+    if (isUndefined(host)) {
+      throw new Error('Host must be set if ingress.enabled is true');
+    }
+    const ing = new k8s.extensions.v1beta1.Ingress(name, {
+      metadata: { annotations },
+      spec: {
+        rules: [
+          {
+            host,
+            http: {
+              paths: [
+                {
+                  path: '/',
+                  backend: {
+                    serviceName: name,
+                    servicePort: port,
+                  },
+                },
+              ],
+            },
+          },
+        ],
+        // TODO: refactor to use new ingress function
+        tls: tls ? [{ hosts: [host], secretName: tls }] : [],
+      },
+    });
+    resources.push(ing);
+  }
+
+  return finalize(resources, { labels: selector, namespace });
+};
+
+export { monerod };
---
+++ apps/monero-node/params.ts
@@ -0,0 +1,44 @@
+import {
+  Image,
+  Ingress,
+  IngressParameter,
+  Name,
+  Namespace,
+  Persistence,
+  PersistenceParameter,
+  Port,
+} from '@dpu/jkcfg-k8s/parameters';
+import { Boolean, String } from '@jkcfg/std/param';
+
+export interface Parameters {
+  name: string;
+  namespace: string;
+  image: string;
+  port: number;
+  ingress: IngressParameter;
+  persistence: PersistenceParameter;
+  sealedSecrets: boolean;
+  secrets: Partial<{
+    walletPass: string;
+    rpcCreds: {
+      user: string;
+      pass: string;
+    };
+  }>;
+}
+
+export const params: Parameters = {
+  name: Name('monerod'),
+  namespace: Namespace('monerod'),
+  image: Image('xmrto/monero:v0.17.0.1'),
+  port: Port(18081),
+  ingress: Ingress(),
+  persistence: Persistence('100Gi'),
+  sealedSecrets: Boolean('sealedsecrets', false)!,
+  secrets: {
+    rpcCreds: {
+      user: String('moneroduser')!,
+      pass: String('monerodpass')!,
+    },
+  },
+};
